db0ea7ec108c27a23c9e3d12d40867459489e88e
Sasha Butenkova <butenkova66@gmail.com>
2024-07-11 15:07:04 +0300

    bugs

--- __test__/e2e/videos.api.test.ts
+++ __test__/e2e/videos.api.test.ts
@@ -1,5 +1,5 @@
 import request from 'supertest'
-import { app } from '../../src/types/app'
+import { app } from '../../app'
 
 describe('videos', () => {
 	beforeAll(async () => {
--- app.ts
+++ app.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express'
-import { getVideoRoutes } from '../routes/videos'
-import { db } from '../repositories/VideosRepository'
-import { getTestRouter } from '../routes/tests'
+import { getVideoRoutes } from './src/routes/videos'
+import { db } from './src/repositories/VideosRepository'
+import { getTestRouter } from './src/routes/tests'
 
 export const app = express()
 
